347b78a6ae5aee9b95eaa751a7d4895420a629ee
zhangziang <ziangzhang_vsama@qq.com>
2023-05-23 19:13:13 +0800

    Initial Commit

---
+++ AsiadCal/AsiadCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsiadCalApp.swift
+//  AsiadCal
+//
+//  Created by viglong on 2023/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AsiadCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
