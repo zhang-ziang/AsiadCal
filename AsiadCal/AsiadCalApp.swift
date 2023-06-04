8d0be9dc9e0a76af631fa3689d281d937302934d
zhangziang <ziangzhang_vsama@qq.com>
2023-06-04 15:27:44 +0800

    intents added

--- AsiadCal/AsiadCalApp.swift
+++ AsiadCal/AsiadCalApp.swift
@@ -6,15 +6,22 @@
 //
 
 import SwiftUI
+import Intents
 
 @main
 struct AsiadCalApp: App {
     @State var matchesData = MatchesData()
+    
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(matchesData)
 //            ProjListView().environmentObject(MatchesData())
+        }.onChange(of: scenePhase){ phase in
+            INPreferences.requestSiriAuthorization({ status in
+                
+            })
         }
     }
 }
---
+++ CalSiri/IntentHandler.swift
@@ -0,0 +1,22 @@
+import Intents
+
+class IntentHandler: INExtension {}
+
+extension IntentHandler: GetRecentEventsIntentHandling{
+    
+    func handle(intent: GetRecentEventsIntent, completion: @escaping(GetRecentEventsIntentResponse)->Void){
+//        guard let amount = intent.currencyAmount?.amount?.doubleValue else{
+//            completion(INRequestPaymentIntentResponse(code: .failure, userActivity: nil))
+//            return
+//        }
+//        number = amount
+        let response : GetRecentEventsIntentResponse
+        let userActivity = NSUserActivity(activityType: NSStringFromClass(GetRecentEventsIntent.self))
+//        response.userActivity = user
+//        print(number)
+//        response = ReceiveSuccessIntentResponse(code: .success, userActivity: userActivity)
+//        completion(INRequestPaymentIntentResponse(code: .success, userActivity: nil))
+        response = GetRecentEventsIntentResponse.success(detail: "some detail here")
+        completion(response)
+    }
+}
